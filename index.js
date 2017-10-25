f0f4fcfe07895438b3e6b882e00ee58ff6f913c0
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-10-24 18:11:46 -0700

    support for fetching from two spreadsheets: Resellers and Suppliers

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 require('dotenv').config()
 const request = require('request')
 const url = require('url') 
-const sheetId = process.env.SHEET_ID
+const resellersSheetId = process.env.RESELLERS_SHEET_ID
+const suppliersSheetId = process.env.SUPPLIERS_SHEET_ID
 const apiKey = process.env.API_KEY
 
 module.exports = (req, res) => {
-  const range = url.parse(req.url, true).query.range
-  request(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`, (error, response, body) => {
-    if(!error) {      
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.end(body)
-    }
-    else
-      console.log(error)
-  })
+  const type = url.parse(req.url, true).query.type
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if(type === 'lojistas') {
+    request(`https://sheets.googleapis.com/v4/spreadsheets/${resellersSheetId}/values/${type}?key=${apiKey}`, (error, response, body) => {
+      if(!error) {      
+        res.end(body)
+      }
+      else
+        console.log(error)
+    })
+  }
+  else if(type === 'fornecedores') {
+    request(`https://sheets.googleapis.com/v4/spreadsheets/${suppliersSheetId}/values/${type}?key=${apiKey}`, (error, response, body) => {
+      if(!error) {      
+        res.end(body)
+      }
+      else
+        console.log(error)
+    })
+  }
+  else
+    res.end('Especifique um tipo: Lojistas ou Fornecedores')
 }
+
+//remember to deploy with --dotenv
